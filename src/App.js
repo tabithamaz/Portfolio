be12f2505ac01ad5e018e666d66920f7a39882c3
tabithamaz <joycemanzanza@gmail.com>
2023-04-03 16:16:22 +0100

    feat: page home

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Header from './components/molecules/Header';
 import "./components/molecules/Header.css";
 import About from "./components/molecules/About";
 import Icones from './components/molecules/Home';
-// import Home from './components/molecules/Home'
+import Home from './components/molecules/Home'
 
 function App() {
   const data = {
-    paragraph1: "Hi, my nama is",
-    title1: "Michel Buhendwa.",
-    title2: "I build things for the web.",
-    paragraph2: "I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement."
+    // paragraph1: "Hi, my nama is",
+    // title1: "Michel Buhendwa.",
+    // title2: "I build things for the web.",
+    // paragraph2: "I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement."
 
 
   }
   return (
     <>
       <Header />
-      {/* <Home /> */}
-      {/* <About data={data} /> */}
+      <Home />
+     
     </>
 
   );
--- src/components/molecules/Home.js
+++ src/components/molecules/Home.js
@@ -6,38 +6,67 @@ import { FiLinkedin} from "react-icons/fi"
 import { AiFillCodepenCircle} from "react-icons/ai"
 
 function Home(){
-    // return(
-    //     <>
-    //     <section id ="home">
-    //     <div className="lefticones">
-    //     <div className="socialsGrid">
-    //                 <a href="" target='_blank'>
-    //                     < AiFillGithub className="icon" /></a>
-    //                 <a>< AiFillInstagram className="icon" /></a>
-    //                 <a><FiTwitter className="icon" /></a>
-    //                 <a><FiLinkedin className="icon" /></a>
-    //                 <a><AiFillCodepenCircle className='icon'/></a>
-
-    //     </div>
-    //     </div>
-    //     </section>
-    //     <div className="container">
-    //     <span className="introText">
-    //         my name is 
-    //     </span>
-    //     <h1 className="title">
-    //     </h1>
-    //     <span className="subTitle">
-    //     </span>
-    //     <span className="">
-    //     </span>
-    //     <p className="homeParagraph">
-    //     </p>
-
-    //     </div>
-    //     </>
+    return(
+        <>
     
-    // )
+            
+            <div className="lefticons">
+                <div className="socialsGrid">
+                    <a href="" target='_blank'>
+                        < AiFillGithub className="icon" /></a>
+                    <a>< AiFillInstagram className="icon" /></a>
+                    <a><FiTwitter className="icon" /></a>
+                    <a><FiLinkedin className="icon" /></a>
+                    <a><AiFillCodepenCircle className='icon' /></a>
+                    <div className="line">
+                    </div>
+                </div>
+                </div> 
+            
+        <section className="sectionHome">
+        <div className="homeContainer">
+        <h1 className="introText">
+        Hi, my name is 
+        </h1> 
+        <h2 className="title">
+            Michel buhendwa
+        </h2>
+        <h3 className="subtitle">
+        I build things for the web.
+        </h3>
+        <p className="homeParagraph">
+                    I'am a software engineer specializing
+                    in building (and occasionally designing)
+                    exceptional digital experiences.
+                    Currently,I'm focused
+                    on building accessible,
+                    human-centered products at <span> upstatement</span>
+        </p>
+                <div className="LowerHomeSection">
+                    <button className="containerBtn">
+                        <a href="#contact" className="flex">
+                            check out my course!
+                        </a>
+                    </button>
+                
+                </div>
+        </div>
+                    <div className="emailAdress">
+                        <a href="https://mailto:michel@kinshasadigital.com" target="_blank" className="lien">
+                            michel@kinshasadigital.com
+                        </a>
+                    
+                    <div className="rightEmail">
+
+                    </div>
+                </div>
+                
+            </section>
+           
+        
+        </>
+    
+    )
     
 
 }
